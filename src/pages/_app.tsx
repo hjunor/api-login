f6e1e897890976ed49d2e4dd905f9a7be1194ba0
hjunor <hjunor@gmail.com>
2022-08-08 17:01:47 -0300

    add: refactor _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import '../styles/global.css';
 import { AppProps } from 'next/app';
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-};
+}
 
 export default MyApp;
