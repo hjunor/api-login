6320d822b2a7442a6bd88d69c52f595a824e8b68
hjunor <hjunor@gmail.com>
2022-08-08 20:09:47 -0300

    add: refactor next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: [
       'www.datocms-assets.com',
